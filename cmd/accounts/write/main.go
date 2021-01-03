e980cd07bb0036b302b1da4ea4368907345f2358
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2021-01-02 22:29:39 -0300

    update docker image

--- cmd/accounts/write/main.go
+++ cmd/accounts/write/main.go
@@ -2,7 +2,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"os"
@@ -23,7 +22,6 @@ const (
 )
 
 func main() {
-	fmt.Println("Accounts write service")
 	port := os.Getenv("PORT")
 	if port != "" {
 		port = defaultPort
